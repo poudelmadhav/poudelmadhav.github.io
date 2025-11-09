824ef93f75b1c5ab85d496f0499c0695967ad4e6
Madhav Paudel <poudelmadhav143@gmail.com>
2025-11-09 21:14:48 +0545

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -180,6 +180,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-H3FLB0ZN91"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-H3FLB0ZN91');
+            `,
+          }}
+        />
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
